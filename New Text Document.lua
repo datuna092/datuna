2c3160e3646d2f8793e6a515c3918199fbfc82e8
datuna092 <mikautadze.datuna@mail.com>
2025-02-03 18:51:41 +0100

    my first coding

---
+++ New Text Document.lua
@@ -0,0 +1,49 @@
+
+let x = 3;
+
+if( x > 10 ) {
+  console.log("More than 10")
+}
+else if( x > 5 ) {
+  console.log("More than 5")
+}
+else {
+  console.log("Less than 5")
+}
+  
+ 
+
+
+let x = 100;
+switch (x) {
+  case 25:
+    console.log("25 ia");
+    break;
+    case 30:
+      console.log("30 ia");
+      break;
+      case 50:
+        console.log("50 ia");
+        break;
+        default:
+          console.log("Neither")
+        
+        
+}
+
+
+
+let x = 11;
+if(x < 0) {
+  console.log("Wrong input");
+}
+else if(x % 2 === 0) {
+  console.log("Even");
+} else{
+  console.log("Odd")
+}
+
+
+ 
+
+
